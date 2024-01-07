a1be1c5d5f1a05b95a7e2408873559dd6f79aaed
Almaz <olmosbek.yusupov@tradecoreprime.com>
2024-01-07 17:51:50 +0500

    Add view model to the home view

--- views/home.py
+++ views/home.py
@@ -1,24 +1,17 @@
 import fastapi
 from fastapi_chameleon import template
+from starlette.requests import Request
 
+from viewmodels.shared.indexviewmodel import IndexViewModel
 
 router = fastapi.APIRouter()
 
 
 @router.get('/')
 @template(template_file='home/index.pt')
-def index():
-    return {
-        'package_count': 274_000,
-        'release_count': 2_234_847,
-        'user_count': 73_874,
-        'packages': [
-            {'id': 'fastapi', 'summary': 'A great web framework'},
-            {'id': 'uvicorn', 'summary': 'You favorite ASGI server'},
-            {'id': 'httpx', 'summary': 'Requests for an async world'},
-        ],
-        'user_name': 'almaz'
-    }
+def index(request: Request):
+    vm = IndexViewModel(request)
+    return vm.to_dict()
 
 
 @router.get('/about')
